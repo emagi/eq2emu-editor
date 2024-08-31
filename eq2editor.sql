7e3434b8e2cac761f1c502493c0083991d0ed987
Emagi <image.emagi@gmail.com>
2024-08-31 11:50:52 -0400

    Update eq2editor.sql

--- eq2editor.sql
+++ eq2editor.sql
@@ -1,41 +1,19 @@
-/*!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.5.1-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: eq2editor
--- ------------------------------------------------------
--- Server version	11.4.2-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
-
 --
 -- Table structure for table `config`
 --
 
 DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `config` (
   `config_name` varchar(250) NOT NULL DEFAULT '',
   `config_value` varchar(250) NOT NULL DEFAULT '',
   `config_explain` varchar(250) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `config`
 --
 
 LOCK TABLES `config` WRITE;
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
 INSERT INTO `config` VALUES
 ('db_version','1.0','Current version of the DB Editor and it\'s eq2editor database. Value used to see what upgrade scripts to run.'),
 ('app_name','',NULL),
